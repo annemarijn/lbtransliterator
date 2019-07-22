a1b9f3eae91f2a48d3f1aa2339389f0f0f6ca29c
annemarijn <anneaarssen@gmail.com>
2019-07-22 12:07:56 +0000

    first commit

---
+++ application.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, request, session
+
+import requests
+
+app = Flask(__name__)
+
+# Credit to this person for inspiration https://stackoverflow.com/questions/14173421/use-string-translate-in-python-to-transliterate-cyrillic
+def translit(text):
+
+    symbols = str.maketrans(u"абвгдеёжзийклмнопрстуѹфцчшъыьѣэѳѵѡАБВГДЕЁЖЗИЙКЛМНОПРСТУѸФЦЧЪЫЬѢЭѲѴѠ",
+                               u"abvgdeёžzijklmnoprstuufcčšʺyʹěėfiôABVGDEËŽZIJKLMNOPRSTUUFCČʺYʹĚĖFIÔ")
+    sequence = {
+        u'х':'ch',
+        u'щ':'šč',
+        u'ю':'ju',
+        u'я':'ja',
+        u'X':'Ch',
+        u'Щ':'Šč',
+        u'Ю':'Ju',
+        u'Я':'Ja'
+    }
+
+    for char in sequence.keys():
+        text = text.replace(char, sequence[char])
+
+    return text.translate(symbols)
+
+
+# Main (index) page
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        cyrillic = request.form.get("cyrillic")
+        transliteration = translit(cyrillic)
+
+        return render_template("index.html", transliteration=transliteration, cyrillic=cyrillic)
+
+    else:
+       return render_template("index.html")
